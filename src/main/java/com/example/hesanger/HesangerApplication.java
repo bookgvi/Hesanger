9fbd585ae3013832a96224fceb2372089b8c6d3e
bookgvi <bookgvi@gmail.com>
2022-11-03 11:55:09 +0300

    git init

---
+++ src/main/java/com/example/hesanger/HesangerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hesanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HesangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HesangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hesanger/HesangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hesanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HesangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
